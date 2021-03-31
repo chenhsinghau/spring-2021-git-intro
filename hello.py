cc739845a34ce0fab820ae5c5426d53abf95280a
CypressN <jnovick@oxy.edu>
2021-03-31 10:36:44 -0700

    Silent Hill 2

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print('Hello, World!')
+print('In my restless dreams, I see that town. Silent Hill. You promised me you\'d take me there again someday. But you never did. Well, I\'m alone there now... In our \'special place\'... Waiting for you...')
